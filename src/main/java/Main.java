1e517d27ba0f6d3bf3da5dc046232d08c276c2ec
Aaron Shaver <AaronShaverPDX@gmail.com>
2018-09-11 12:06:45 -0700

    Simple animation, and tweak timing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,14 @@ package main.java;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        String characters = "->";
+
         for (int i = 0; i < 50; i++) {
             Display.ClearScreen();
-            System.out.println(i);
-            Thread.sleep(200);
+            String output = new String(new char[i]).replace("\0", " ");
+            output += characters;
+            System.out.println(output);
+            Thread.sleep(190);
         }
     }
 }
